e9633e1f53314261bc4f95baba61516c87d2be83
Chitsanupong Rongpan <pp.rongpan@gmail.com>
2022-12-10 18:49:50 +0700

    Add file input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 using namespace std;
 void listelement(const vector<string> &vec, string sep = " ", bool debug = false) {
   // Iterating over all elements of vector
@@ -21,21 +22,51 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  // Sample debug code
-  string samplecode = "VAR X = 0;PRINT X;";
-  vector<string> codelist{};
-  string buffer = "";
-  for (int i = 0; i < samplecode.length(); i++) {
-    if (samplecode[i] == ';') {
-      codelist.push_back(buffer);
-      buffer = "";
-    } else {
-      buffer = buffer + samplecode[i];
-      if (isDebug) cout << buffer << endl;
+  // Check if the "--file" flag was provided
+  bool fileSpecified = false;
+  for (int i = 0; i < argc; i++) {
+    if (string(argv[i]) == "--file") {
+      fileSpecified = true;
+      if (i + 1 < argc) {
+        // Read the code from the file specified by the next argument
+        ifstream infile(argv[i + 1]);
+        if (infile) {
+          // Parse the code from the file
+          string samplecode = string((istreambuf_iterator<char>(infile)),
+                                     istreambuf_iterator<char>());
+          infile.close();
+          vector<string> codelist{};
+          string buffer = "";
+          for (int i = 0; i < samplecode.length(); i++) {
+            if (samplecode[i] == ';') {
+              codelist.push_back(buffer);
+              buffer = "";
+            } else {
+              buffer = buffer + samplecode[i];
+              if (isDebug) cout << buffer << endl;
+            }
+          }
+          cout << codelist.size() << endl;
+          listelement(codelist, ", ", isDebug);
+        } else {
+          // Failed to open the file
+          cerr << "Error: Failed to open file '" << argv[i + 1] << "'" << endl;
+          return 1;
+        }
+      } else {
+        // The "--file" flag was provided, but no file was specified
+        cerr << "Error: No file specified for '--file' flag" << endl;
+        return 1;
+      }
     }
   }
-  cout << codelist.size() << endl;
-  listelement(codelist, ", ", isDebug);
+
+  if (!fileSpecified) {
+    // The "--file" flag was not provided
+    cerr << "Error: No file specified. Use the '--file' flag to specify a file to parse." << endl;
+    return 1;
+  }
+
   return 0;
 }
 
