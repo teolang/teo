43b67416a665e99bba196d44af4a98f0d982c47d
Chitsanupong Rongpan <pp.rongpan@gmail.com>
2022-12-11 13:02:59 +0700

    Finish adding goto

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 all: main
 
-main: main.cpp parse_file.cpp interpret.cpp
+main: main.cpp parse_file.cpp interpret.cpp goto.cpp end.cpp let.cpp print.cpp
 	clang++ main.cpp parse_file.cpp interpret.cpp -o main
 
 
-run: main
+run: main test/test.teo
 	./main --file test/test.teo
 
 clean:
--- goto.cpp
+++ goto.cpp
@@ -1,31 +1,16 @@
 #include <algorithm>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
-#include <map>
 
-// Definition of the tok_goto function
-void tok_goto(const std::string &line,
-              std::map<std::string, int> &vars,
-              const std::vector<std::string> &code_lines,
-              int &current_line,
+void tok_goto(const std::string &line, std::map<std::string, int> &vars,
+              const std::vector<std::string> &code_lines, int &current_line,
               const bool is_debug) {
-  // Extract the label specified after the "goto " keyword
-  std::string label = line.substr(5);
-
-  // Search for a line that starts with the label followed by a colon (":")
-  auto it = std::find_if(code_lines.begin(), code_lines.end(),
-                         [&label](const std::string &s) {
-                           return s.find(label + ":") == 0;
-                         });
-  if (it != code_lines.end()) {
-    // If the line is found, set the current_line variable to the index of the line
-    current_line = std::distance(code_lines.begin(), it);
-  } else {
-    // If the line is not found, print an error message if debug mode is enabled
-    if (is_debug) {
-      std::cerr << "Error: Unknown label '" << label << "'" << std::endl;
-    }
-  }
+  // Parse the line to extract the label to go to
+  std::string label =
+      line.substr(5); // 5 is the length of "goto "
+                      // The label was found in the code, so set the
+                      // current_line variable to the line number of the label
+  current_line = std::stoi(label);
 }
-
