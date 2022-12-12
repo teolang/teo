274e745f0124d17a2021d26f5f26bb59db860fbf
Chitsanupong Rongpan <pp.rongpan@gmail.com>
2022-12-12 18:07:50 +0700

    Adding stdin flags

--- parse_file.cpp
+++ parse_file.cpp
@@ -13,7 +13,12 @@ vector<string> parse_file(int argc, char *argv[], bool isDebug) {
   vector<string> codelist{};
   bool fileSpecified = false;
   for (int i = 0; i < argc; i++) {
-    if (string(argv[i]) == "--file") {
+    if (string(argv[i]) == "--stdin") {
+      // Read the code from stdin
+      string samplecode =
+          string((istreambuf_iterator<char>(cin)), istreambuf_iterator<char>());
+      codelist = parsehandler(samplecode, isDebug);
+    } else if (string(argv[i]) == "--file") {
       fileSpecified = true;
       if (i + 1 < argc) {
         // Read the code from the file specified by the next argument
