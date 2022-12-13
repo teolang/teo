6767b0ff2377bde8bacdf2335e3dbbeaa3d06ef4
Chitsanupong Rongpan <pp.rongpan@gmail.com>
2022-12-13 19:49:33 +0700

    Adding basic assert command with goto

---
+++ assert.cpp
@@ -0,0 +1,23 @@
+#include <map>
+#include <sstream>
+#include <string>
+#include <iostream>
+
+void tok_assert(const std::string &line, std::map<std::string, int> &vars,
+                const bool is_debug) {
+  std::string commands = line.substr(7);
+  std::stringstream ss(commands);
+  std::string variable[2];
+  std::string goifassert;
+  std::getline(ss, variable[0], ' ');
+  std::getline(ss, variable[1], ' ');
+  std::getline(ss, goifassert);
+  if (vars.find(variable[0]) == vars.end() || vars.find(variable[1]) == vars.end()){
+      if (is_debug) {
+          std::cerr << "Error: Unknown variable '" << variable[0] << "'" << std::endl;
+      }
+  } else {
+  if (vars.at(variable[0]) == vars.at(variable[1]) && is_debug){
+      std::cout << "Going to line " << goifassert << std::endl;
+  }}
+}
--- interpret.cpp
+++ interpret.cpp
@@ -10,6 +10,7 @@
 #include "goto.cpp"
 #include "let.cpp"
 #include "print.cpp"
+#include "assert.cpp"
 
 // Definition of the interpret function
 void interpret(const std::vector<std::string> &code_lines,
@@ -31,6 +32,8 @@ void interpret(const std::vector<std::string> &code_lines,
       // Pass a reference to the code_lines and current_line variables
       tok_goto(line, vars, code_lines, current_line, is_debug);
       gotoed = true;
+    } else if (line.find("assert ") == 0){
+        tok_assert(line, vars, is_debug);
     } else if (line.find("end") == 0) {
       // Call the tok_end function defined in the end.cpp file
       tok_end(line, is_debug);
