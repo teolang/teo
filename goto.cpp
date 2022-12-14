fc355fbd7c502da22c5b94276bdbf444cceee4ec
Chitsanupong Rongpan <pp.rongpan@gmail.com>
2022-12-14 18:57:43 +0700

    Fixing goto not doing anything

--- goto.cpp
+++ goto.cpp
@@ -8,13 +8,17 @@
 void tok_goto(const std::string &line, std::map<std::string, int> &vars,
               const std::vector<std::string> &code_lines, int &current_line,
               const bool is_debug) {
-  // Parse the line to extract the label to go to
-  std::string label =
-      line.substr(5); // 5 is the length of "goto "
-                      // The label was found in the code, so set the
-                      // current_line variable to the line number of the label
-  current_line = std::stoi(label);
-  if (is_debug) {
-    std::cout << "Going to line " << label << " : " << code_lines[std::stoi(label)] << std::endl;
+      // Extract the label specified after the "goto " keyword
+  std::string label = line.substr(5);
+  int it = std::stoi(label);
+  // Search for a line that starts with the label followed by a colon (":")
+  if (it < code_lines.size()) {
+    // If the line is found, set the current_line variable to the index of the line
+    current_line = it - 1;
+  } else {
+    // If the line is not found, print an error message if debug mode is enabled
+    if (is_debug) {
+      std::cerr << "Error: Unknown label '" << label << "'" << std::endl;
+    }
   }
 }
