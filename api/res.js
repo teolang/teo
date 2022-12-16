5398ba1d7ef690d549454ab5c9353458faeafea9
Chitsanupong Rongpan <pp.rongpan@gmail.com>
2022-12-16 18:29:26 +0700

    Adding basic api for connecting with web client

---
+++ api/res.js
@@ -0,0 +1,38 @@
+const fs = require('fs'); // For file manipulation
+const crypto = require('crypto'); // For generating the file name
+const express = require('express'); // For creating the REST API
+const { exec } = require('child_process'); // For running the `echo` command
+
+const app = express();
+
+app.get('/echo', (req, res) => {
+  // Get the query from the request
+  const query = req.query.q.replace(/:/g, ';');
+
+  // Generate a file name based on the hash of the query
+  const fileName = crypto.createHash('sha256').update(query).digest('hex');
+
+  // Write the query to the file
+  fs.writeFileSync(fileName, query);
+
+  // Run the `echo` command and read the output
+  exec(`../main --debug --file ./${fileName}`, (err, stdout, stderr) => {
+    if (err) {
+      // If there was an error, return the error message and a non-zero exit code
+      res.send({ output: stderr, code: err.code });
+    } else {
+      // If the command ran successfully, return the output and a zero exit code
+      // Replace semicolons with colons and newline characters with <br>
+      const output = stdout.replace(/\n/g, '<br>');
+      res.send({ output, code: 0 });
+
+      // Delete the file after the response is sent
+      fs.unlinkSync(fileName);
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Echo API listening on port 3000!');
+});
+
