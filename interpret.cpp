81ee769795feedd38f346c1176799c8beb3ac452
Chitsanupong Rongpan <pp.rongpan@gmail.com>
2022-12-28 19:34:32 +0700

    Move commands to enum.

--- interpret.cpp
+++ interpret.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <iostream>
 #include <map>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -14,16 +15,63 @@
 
 #include "cparse/shunting-yard.h"
 
+enum commandlist {
+  Let,
+  Print,
+  Goto,
+  Assert,
+  End,
+};
+
 // Definition of the interpret function
 void interpret(const std::vector<std::string> &code_lines,
                const bool is_debug) {
   cparse::TokenMap vars;
+  std::map<std::string, commandlist> commands;
+  commands["let"] = Let;
+  commands["print"] = Print;
+  commands["goto"] = Goto;
+  commands["assert"] = Assert;
+  commands["end"] = End;
   int current_line = 0;
   bool gotoed = false;
   while (current_line < code_lines.size()) {
     gotoed = false;
     const auto &line = code_lines[current_line];
+    std::stringstream ss(line);
+    std::string command;
+    ss >> command;
     std::cout << "Running '" << line << "'" << std::endl;
+    std::map<std::string, commandlist>::iterator commandit =
+        commands.find(command);
+    if (commandit == commands.end()) {
+      std::cerr << "Command not found!" << std::endl;
+      exit(1);
+    } else {
+      switch (commandit->second) {
+      case Let:
+        tok_let(line, vars, is_debug);
+        break;
+      case Print:
+        tok_print(line, vars, is_debug);
+        break;
+      case Goto:
+        tok_goto(line, code_lines, current_line, vars, is_debug);
+        gotoed = true;
+        break;
+      case Assert:
+        if (tok_if(line, vars, is_debug) != -1) {
+          tok_goto("goto " + std::to_string(tok_if(line, vars, is_debug)),
+                   code_lines, current_line, vars, is_debug);
+          gotoed = true;
+        }
+        break;
+      case End:
+        tok_end(line, is_debug);
+        break;
+      }
+    }
+    /*
     if (line.find("let ") == 0) {
       // Call the tok_let function defined in the let.cpp file
       tok_let(line, vars, is_debug);
@@ -49,6 +97,7 @@ void interpret(const std::vector<std::string> &code_lines,
         std::cerr << "Error: Unknown command '" << line << "'" << std::endl;
       }
     }
+    */
     // Increment the current_line variable unless it was set by the tok_goto
     // function
     if (!gotoed) {
