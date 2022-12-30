78384a1f82aea2dd9ad3ed2911eec1242c54ada3
Chitsanupong Rongpan <pp.rongpan@gmail.com>
2022-12-30 17:31:31 +0700

    Remove old code

--- interpret.cpp
+++ interpret.cpp
@@ -71,33 +71,6 @@ void interpret(const std::vector<std::string> &code_lines,
         break;
       }
     }
-    /*
-    if (line.find("let ") == 0) {
-      // Call the tok_let function defined in the let.cpp file
-      tok_let(line, vars, is_debug);
-    } else if (line.find("print ") == 0) {
-      // Call the tok_print function defined in the print.cpp file
-      tok_print(line, vars, is_debug);
-    } else if (line.find("goto ") == 0) {
-      // Call the tok_goto function defined in the goto.cpp file
-      // Pass a reference to the code_lines and current_line variables
-      tok_goto(line, code_lines, current_line, vars, is_debug);
-      gotoed = true;
-    } else if (line.find("assert ") == 0) {
-      if (tok_if(line, vars, is_debug) != -1) {
-        tok_goto("goto " + std::to_string(tok_if(line, vars, is_debug)),
-                 code_lines, current_line, vars, is_debug);
-        gotoed = true;
-      }
-    } else if (line.find("end") == 0) {
-      // Call the tok_end function defined in the end.cpp file
-      tok_end(line, is_debug);
-    } else {
-      if (is_debug) {
-        std::cerr << "Error: Unknown command '" << line << "'" << std::endl;
-      }
-    }
-    */
     // Increment the current_line variable unless it was set by the tok_goto
     // function
     if (!gotoed) {
