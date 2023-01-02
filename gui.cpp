1db5d3aa2226722695dfa366caa0f05a5c2bf404
Chitsanupong Rongpan <pp.rongpan@gmail.com>
2023-01-02 12:42:41 +0700

    gui: Setting default window size.

--- gui.cpp
+++ gui.cpp
@@ -81,6 +81,7 @@ int startGUI(int argc, char *argv[]) {
   // Create the window
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title(GTK_WINDOW(window), "Text Editor");
+  gtk_window_set_default_size(GTK_WINDOW(window), 500, 500);
   g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
   // Create the box
